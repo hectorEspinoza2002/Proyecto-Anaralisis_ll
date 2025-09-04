63334b100d507321ab8e6d35d1487076b98aafd0
Francisco Silva <metroproyect@gmail.com>
2025-09-04 17:39:49 -0600

    Mejos en crud user

--- angularprj/frontalm5/src/app/component/addusuario/addusuario.component.ts
+++ angularprj/frontalm5/src/app/component/addusuario/addusuario.component.ts
@@ -8,6 +8,8 @@ import { Genero } from '../../entity/genero';
 import { StatusUsuario } from '../../entity/statusUsuario';
 import { Sucursal } from '../../entity/Sucursal';
 import { Role } from '../../entity/Role';
+import { SucursalService } from '../../service/sucursal.service';
+import { RoleService } from '../../service/role.service';
 
 @Component({
   selector: 'app-addusuario',
@@ -18,25 +20,113 @@ import { Role } from '../../entity/Role';
 export class AddusuarioComponent implements OnInit {
   usuario = new Usuario();
 
-  generosDisponibles: Genero[] = [];
-  statusDisponibles: StatusUsuario[] = [];
-  sucursalesDisponibles: Sucursal[] = [];
-  rolesDisponibles: Role[] = [];
-  mensaje: String = '';
-  reglasEmpresa: any = {};
+  selectedStatus!: number;
+  status: StatusUsuario [] = [];
 
   selectedGenero!: number;
-  selectedStatus!: number;
-  selectedSucursal!: number;
-  selectedRole!: number;
+  genero: Genero [] = [];
 
-  constructor(private userService: UsuarioService, private router: Router) {}
+  selectedSucursal!: number;
+  sucursal: Sucursal [] = [];
+
+  selectedRole!: number;
+  rol: Role [] = [];
+
+  mensaje: String = '';
+
+  constructor(private userService: UsuarioService,
+    private router: Router,
+    private statusService:StatusUsuarioService,
+    private generoService:GeneroService,
+    private sucursalService:SucursalService,
+    private roleService:RoleService
+  ) {}
 
   ngOnInit(): void {
     this.cargarDatosIniciales();
   }
 
+  Cancelar(){
+    this.router.navigate(["listusuarios"]);
+  }
+
+  guardarUser(user:Usuario){
+    this.usuario.idStatusUsuario = {idStatusUsuario: this.selectedStatus} as unknown as StatusUsuario;
+    this.usuario.idGenero = {idGenero: this.selectedGenero} as unknown as Genero;
+    this.usuario.idSucursal = {idSucursal: this.selectedSucursal} as unknown as Sucursal;
+    this.usuario.idRole = {idRole: this.selectedRole} as unknown as Role;
+
+    if(
+      typeof user.nombre != "undefined" &&
+      typeof user.apellido != "undefined" &&
+
+      this.selectedStatus &&
+      this.selectedGenero &&
+      this.selectedSucursal &&
+      this.selectedRole ){
+
+      this.userService.addUsuario(user).subscribe({
+        next: (result) => {
+          if(result != null){
+            alert("Usuario: "+user.nombre+ " "+user.apellido+" ingresado correctamente!");
+            this.router.navigate(["listusuarios"]);
+          }
+          this.resetForm();
+        },
+        error:(error) => {
+          alert("Ocurrio un error al guardar el usuario");
+        }
+      })
+    }
+  }
+
+  resetForm(): void{
+    this.usuario = new Usuario;
+    this.selectedStatus = null!;
+    this.selectedGenero = null!;
+    this.selectedSucursal = null!;
+    this.selectedRole = null!;
+  }
+
   cargarDatosIniciales(): void {
     // Cargar géneros
+
+    this.statusService.getAll().subscribe({
+      next: (data) => {
+        this.status = data;
+      },
+      error:(err) => {
+        this.mensaje = 'Error al traer el status usuario';
+      }
+    })
+
+    this.generoService.getAll().subscribe({
+      next: (d) => {
+        this.genero = d;
+      },
+      error: (err) => {
+        this.mensaje = 'Error al traer el genero';
+      }
+    })
+
+    this.sucursalService.getAll().subscribe({
+      next: (a) => {
+        this.sucursal = a;
+      },
+      error: (err) => {
+        this.mensaje = 'Error al traer la sucursal';
+      }
+    })
+
+    this.roleService.getAll().subscribe({
+      next: (t) => {
+        this.rol = t;
+      },
+      error: (err) => {
+        this.mensaje = 'Error al traer el rol';
+      }
+    })
+
+
   }
 }
