2c7051c8bd3a918c4a493cce94720f923932636a
Francisco Silva <metroproyect@gmail.com>
2025-09-11 17:50:31 -0600

    Combo Opcion Agregado

--- angularprj/frontalm5/src/app/component/addopcion/addopcion.component.ts
+++ angularprj/frontalm5/src/app/component/addopcion/addopcion.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Opcion } from '../../entity/opcion';
 import { OpcionService } from '../../service/opcion.service';
 import { Router } from '@angular/router';
+import { Menu } from '../../entity/menu';
+import { MenuService } from '../../service/menu.service';
 
 @Component({
   selector: 'app-addopcion',
@@ -10,26 +12,54 @@ import { Router } from '@angular/router';
   styleUrl: './addopcion.component.css'
 })
 export class AddopcionComponent {
-  opn = new Opcion;
+  opcion = new Opcion;
 
-  constructor(private service:OpcionService, private router:Router){}
-  ngOnInit(): void{}
+  selectedMenu!: number;
+  menu: Menu[] = [];
+  mensaje: String = '';
+
+  constructor(private service:OpcionService, private router:Router,
+    private menuService:MenuService
+  ){}
+  ngOnInit(): void{
+    this.cargarMenu();
+  }
 
   Cancelar(){
     this.router.navigate(["listopcion"]);
   }
 
   guardar(opc:Opcion){
-    if(typeof(opc.nombre) != "undefined"){
+
+    this.opcion.menu = {idMenu: this.selectedMenu} as unknown as Menu;
+
+    if(typeof(opc.nombre) != "undefined" && this.selectedMenu){
       this.service.addOpcion(opc).subscribe(result => {
         if(result != null){
           alert("Rol: "+opc.nombre+" ingresado correctamente!");
           this.router.navigate(["listopcion"]);
         }
+        this.resetForm();
       });
     } else{
       alert("Debe ingresar datos!");
     }
+  }
+
+  resetForm(): void{
+    this.opcion = new Opcion;
+    this.selectedMenu = null!;
+  }
+
+  cargarMenu(): void{
+    this.menuService.getAll().subscribe({
+      next: (data) => {
+        this.menu = data;
+      },
+      error: (err) => {
+        this.mensaje = 'Error al traer menus';
+      }
+    })
   }
 
 }
