ab38ee366ff45bbcf44f62630118cef65bf8424c
Héctor Espinoza <hectorfrancisco1214@gmail.com>
2025-09-11 22:57:45 -0600

    ruta opciones compuestas

--- angularprj/frontalm5/src/app/service/opcion.service.ts
+++ angularprj/frontalm5/src/app/service/opcion.service.ts
@@ -4,10 +4,9 @@ import { Observable } from 'rxjs';
 import { Opcion } from '../entity/opcion';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class OpcionService {
-
   constructor(private http: HttpClient) {}
   Url = 'http://localhost:9090';
 
@@ -19,14 +18,17 @@ export class OpcionService {
     return this.http.get<any[]>(this.Url + '/list_opciones');
   }
 
-  addOpcion(o:Opcion){
-      return this.http.post<Opcion>(this.Url+"/create_role",o);
-    }
+  addOpcion(o: Opcion) {
+    return this.http.post<Opcion>(this.Url + '/create_opcion', o);
+  }
+
+  editOpcion(id:String, updateOpc:Opcion){
+        return this.http.put<Opcion>(this.Url+"/update_opcion/"+id,updateOpc);
+      }
 
   deleteOpcion(opc: Opcion) {
-    return this.http.delete(
-      this.Url + '/delete_mudulos/' + opc.idOpcion,
-      { responseType: 'text' }
-    );
+    return this.http.delete(this.Url + '/delete_opciones/' + opc.idOpcion, {
+      responseType: 'text',
+    });
   }
 }
