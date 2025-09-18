280de0141381d172909c4907e6e097fb1e29f8f9
Francisco Silva <metroproyect@gmail.com>
2025-09-18 16:36:57 -0600

    Repository F2 listos!

--- alfa/src/main/java/com/proyecto_analisis/alfa/model/repositoryf2/DocumentoPersonaRepository.java
+++ alfa/src/main/java/com/proyecto_analisis/alfa/model/repositoryf2/DocumentoPersonaRepository.java
@@ -1,11 +1,21 @@
 package com.proyecto_analisis.alfa.model.repositoryf2;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.proyecto_analisis.alfa.model.entityf2.DocumentoPersona;
 
 @Repository
-public interface DocumentoPersonaRepository extends JpaRepository<DocumentoPersona, Integer>{
+public interface DocumentoPersonaRepository extends JpaRepository<DocumentoPersona, Integer> {
+
+    // Buscar todas las opciones asignadas a un rol
+    List<DocumentoPersona> findByTipoDocumento_IdTipoDocumento(Integer idTipoDocumento);
+
+    // Buscar si una opcion especifica pertenece a un rol
+    Optional<DocumentoPersona> findByTipoDocumento_IdTipoDocumentoAndPersona_IdPersona(Integer idTipoDocumento,
+            Integer idPersona);
 
 }
